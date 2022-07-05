90f6e284f4acceb08115bb9d993abe6b74b22220
Hafidh Sajid <hafidhsajid@gmail.com>
2022-07-05 17:55:32 +0700

    kuliner

--- app/Http/Controllers/API/API.php
+++ app/Http/Controllers/API/API.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kuliner;
 use App\Models\Wahana;
 use Illuminate\Http\Request;
 
@@ -14,5 +15,11 @@ class API extends Controller
         $wahana  = new Wahana();
         $wahana  = $wahana->get();
         return response()->json($wahana);
+    }
+    public function kuliner()
+    {
+        $kuliner = new Kuliner();
+        $kuliner = $kuliner->get();
+        return response()->json($kuliner);
     }
 }
